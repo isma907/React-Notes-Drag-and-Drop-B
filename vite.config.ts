1d1d4c62c5e7980b310e42350ec79b01a6679649
Ismael <isma907@gmail.com>
2026-04-23 22:24:22 -0300

    added github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/React-Notes-Drag-and-Drop-B/",
   plugins: [react()],
   test: {
     environment: "jsdom",
